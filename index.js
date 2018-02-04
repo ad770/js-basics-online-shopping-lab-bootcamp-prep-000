92031e789465e656af27b8039192a95513b31983
arye dov eidelman <aryedov.e@gmail.com>
2018-02-04 23:09:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,53 @@ function setCart(c) {
   return cart;
 }
 
+function rand(num){return Math.floor(Math.random()*num+1);}
+
 function addToCart(item) {
- // write your code here
+  cart.push({[item]: rand(100)});
+  console.log(`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+
+  if (cart.length < 1) {
+    console.log('Your shopping cart is empty.');
+  }else{
+    var result = 'In your cart, you have';
+    var key = [];
+    for (var i = 0; i < cart.length; i++) {
+      key = Object.keys(cart[i])[0]
+
+      
+
+
+
+
+
+
+
+      if(i < cart.length-1){
+        if (cart.length = 2) {
+          result +=` ${key} at $${cart[i][key]}`;
+        }else{
+          result +=` ${key} at $${cart[i][key]},`;
+        }
+      }else if(cart.length>1){
+        result +=` and ${key} at $${cart[i][key]}.`;
+      }else {
+        result +=` ${key} at $${cart[i][key]}.`;
+      }
+    }
+    console.log(result);
+  }
 }
+
+// addToCart('bananos');
+// addToCart('g');
+// addToCart('j');
+// addToCart('apples');
+viewCart();
 
 function total() {
   // write your code here
@@ -27,4 +67,5 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+
 }
