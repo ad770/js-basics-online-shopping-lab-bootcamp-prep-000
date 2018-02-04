d96547f266c0990f9a06af871ec5a0fbc44eaf00
arye dov eidelman <aryedov.e@gmail.com>
2018-02-04 23:27:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function viewCart() {
       var key1 = Object.keys(cart[1])[0];
       result = `In your cart, you have ${key0} at $${cart[0][key]} and ${key0} at $${cart[1][key1]}.`;
       break;
-    case > 2:
+    case (> 2):
       var result = 'In your cart, you have';
       var key = [];
       for (var i = 0; i < cart.length; i++) {
