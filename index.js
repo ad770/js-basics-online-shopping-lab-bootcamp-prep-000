17e1d6ae0f6e6003b9042d7161d029ee6bb39e2b
arye dov eidelman <aryedov.e@gmail.com>
2018-02-05 00:19:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  }else{
+    console.log(`Sorry, we don't have a credit card on file for you.`);
+  }
 
 }
 
